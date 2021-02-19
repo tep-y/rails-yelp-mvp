0672580002a83cd6bbddd643017841ff35185c21
tep yith <tep.yith@gmail.com>
2021-02-18 19:16:42 -0500

    Added Restaurant Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :restaurants, only: [:index, :new, :create, :show]
 end
+
+# A visitor can see the list of all restaurants.
+# GET "restaurants"
+
+# A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+# GET "restaurants/new"
+# POST "restaurants"
+
+# A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+# GET "restaurants/38"
+
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
